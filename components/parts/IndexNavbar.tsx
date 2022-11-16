5fc11659fed5c57cfbfc126dc68edb42b10310b7
khistynh <khistynh@gmail.com>
2022-11-16 20:45:36 +0700

    add

---
+++ components/parts/IndexNavbar.tsx
@@ -0,0 +1,18 @@
+
+export default function IndexNavbar() {
+  return (
+    <>
+      {/* <nav className="bg-transparant absolute top-0 left-0 w-full flex item-center z-10">
+        <div className="container">
+          <ul className="flex flex-col">
+            <li>Home</li>
+            <li>About</li>
+            <li>khistynh</li>
+            <li>Porftfolio</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </nav> */}
+    </>
+  )
+}
